6f65d674428ddcbff85ad6378a7e6c25182c3b89
Katczajk <43179276+Katczajk@users.noreply.github.com>
2020-05-31 02:25:15 +0200

    changes & comment

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -5,6 +5,8 @@ int main(int argc, char *argv[])
 {
     /* std::cout << argv[1] << std::endl; */
 
+    /* loop here until control + c */
+    /* rewrite code for using on linux, first testrun control+c dont work correctly */
     for ( ; ; )
     {
         run();
@@ -14,9 +16,12 @@ int main(int argc, char *argv[])
 
 int run()
 {
+    /* game server steps here */
     std::system("notepad.exe");
+    /* message for interrupt */
     std::cout << "Server stoped or crashed - restarting ..." << std::endl;
     std::cout << "Press Control + C to interrupt !" << std::endl;
+    /* simple wait step */
     std::system("ping -n 10 127.0.0.1 >> nul");
     return NULL;
 }
--- Server/Server.h
+++ Server/Server.h
@@ -1,3 +1,8 @@
 #pragma once
+/* include guard here */
+#ifndef server_h
+	#define server_h
+#endif // !server_h
 
+/* init for function run */
 int run();
